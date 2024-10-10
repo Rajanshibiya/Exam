0f65a7ac2d02f8b20b8dd04f0da738d01126128f
Shibiya M. <UT01273@unicomtic.com>
2024-10-10 11:52:36 +0530

    my Exam

---
+++ BookRebtalManagementSystem/BookRebtalManagementSystem/BookManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRebtalManagementSystem
+{
+    internal class BookManager
+    {
+        private List<Book> books = new List<Book>();
+        private int nextId = 1;
+
+        public void createBook(int id, string title, string author, decimal price)
+        {
+            var book = new Book();
+          
+            books.Add(book);
+            Console.WriteLine("Book added successfully.");
+        }
+        public void RentalBooks()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books available.");
+                return;
+            }
+            Console.WriteLine("Books available:");
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+        public void UpdateBook(int bookId, string newTitle,string newAuthor,decimal newRentalPrice)
+        {
+            var book=books.FirstOrDefault(b=>b.BookId == bookId);
+            if (book != null)
+            {
+                books.Remove(book);
+                Console.WriteLine("Book deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Book not found");
+            }
+        }
+    }
+}
---
+++ BookRebtalManagementSystem/BookRebtalManagementSystem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRebtalManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {     BookManager maneger = new BookManager();
+            int choice;
+            do
+            {
+                Console.WriteLine("Book Rental Management System:");
+                Console.WriteLine("1.Add All Book");
+                Console.WriteLine("2.View All Books");
+                Console.WriteLine("3.Update a Book");
+                Console.WriteLine("4.Delete a Book");
+                Console.WriteLine("5.Exit");
+                Console.WriteLine("Choose an option:");
+
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+
+                        break;
+
+                    case 2:
+                        BookManager.ReadBooks();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter Book ID to update:");
+                        var bookIdToUpdate = int.Parse(Console.ReadLine());
+                        Console.write
+
+                }
+            }
+
+        }
+
+
+            static void AddBook(BookManager booKManeger)
+            {
+            Console.WriteLine("Enter Movie Id: ");
+            var id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Title:");
+                var title = Console.ReadLine();
+                Console.WriteLine("Enter Author:");
+                var author = Console.ReadLine();
+                Console.WriteLine("Enter Rental Price:");
+                var rentalPrice = decimal.Parse(Console.ReadLine());
+            bookManager.createBook(id,title, author, rentalPrice);
+            }
+    
+            
+       
+          
+        
+        
+    }
+}
